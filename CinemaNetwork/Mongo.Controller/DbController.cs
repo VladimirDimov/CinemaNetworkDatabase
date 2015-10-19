4fb3372ca6f8ec882a745a93a4ca2580156518ea
VladimirDimov <vdimov1124@gmail.com>
2015-10-19 16:02:15 +0300

    add random generator

---
+++ CinemaNetwork/Mongo.Controller/DbController.cs
@@ -0,0 +1,32 @@
+﻿namespace MongoToSql
+{
+    using MongoDB.Driver;
+    using MongoToSql.Entities;
+    using System.Collections.Generic;
+
+    public class DbController
+    {
+        private string dbName;
+        private string host;        
+
+        public DbController(string dbName, string host)
+        {
+            this.dbName = dbName;
+            this.host = host;
+        }
+
+        public List<PersonMongo> GetPersonDataCollection()
+        {
+            IMongoDatabase db = GetDatabase();
+            var collection = db.GetCollection<PersonMongo>("Person");
+            var personCollection = collection.Find(x => true).ToListAsync<PersonMongo>().Result;
+            return personCollection;
+        }
+
+        public IMongoDatabase GetDatabase()
+        {
+            var mongoCllient = new MongoClient(host);
+            return mongoCllient.GetDatabase(dbName);
+        }
+    }
+}
---
+++ CinemaNetwork/RandomGenerator.Data/RandomGeneratorController.cs
@@ -0,0 +1,54 @@
+﻿namespace RandomGenerators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RandomGeneratorController
+    {
+        private static Random random = new Random();
+        private const string Capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string SmallLetters = "abcdefghijklmnopqrstuvwxyz";
+
+        public int GetRandomInt(int minValue, int maxValue)
+        {
+            return random.Next(minValue, maxValue + 1);
+        }
+
+        public string GetRandomString(int minLength, int maxLength, bool CapitalizeFirstLetter = false)
+        {
+            var length = random.Next(minLength, maxLength + 1);
+            var output = new StringBuilder();
+            var numberOfSmallLetters = SmallLetters.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                output.Append(SmallLetters[random.Next(0, numberOfSmallLetters)]);
+            }
+
+            if (CapitalizeFirstLetter)
+            {
+                output[0] = char.ToUpper(output[0]);
+            }
+
+            return output.ToString();
+        }
+
+        public DateTime GetRandomDateTime(int? fromYear = null, int? toYear = null)
+        {
+            fromYear = fromYear ?? 1920;
+            toYear = toYear ?? 2050;
+            var year = random.Next((int)fromYear, (int)toYear + 1);
+            var month = random.Next(1, 13);
+            var day = random.Next(1, 29);
+            var hour = random.Next(1, 24);
+            var minute = random.Next(1, 60);
+            var seconds = random.Next(1, 60);
+            var milliseconds = random.Next(1, 100);
+
+            return new DateTime(year, month, day, hour, minute, seconds, milliseconds);
+        }
+    }
+}
