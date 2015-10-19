2c907b4e52e4bafea0aeb0505289d3d1925a87c8
Kiril Borisov <borisov.kiril760@gmail.com>
2015-10-20 01:55:53 +0300

    Excel application added. Used for importing data from .xls files (version 2003 and before support presents) plus sample data for towns. NB! If you want to test functionality please delete all records from Towns table in CinemaNetworkEntities database and run the console application again. Note that towns id's may begin not from 1 due to all the tests applied before releasing this functionality

---
+++ CinemaNetwork Second Edition/ExcelImporterConsole.Controller/ExcelImporter.cs
@@ -0,0 +1,41 @@
+﻿namespace ExcelImporterConsole.Controller
+{
+    using System.Data;
+    using System.Data.OleDb;
+    using System.Linq;
+    using CinemaNetwork.Models;
+
+    public class ExcelImporter
+    {
+        public static void ImportTowns(string filePath)
+        {
+            var currentDb = new CinameNetworkEntities();
+
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + filePath + "';" +
+                         "Extended Properties=\"Excel 8.0;HDR=NO;IMEX=1\";";
+            OleDbConnection MyConnection = new OleDbConnection(connectionString);
+            DataSet DtSet;
+            OleDbDataAdapter MyCommand;
+            MyCommand = new OleDbDataAdapter("select * from [Towns$]", MyConnection);
+            MyCommand.TableMappings.Add("Table", "TestTable");
+            DtSet = new DataSet();
+            MyCommand.Fill(DtSet);
+
+
+            currentDb.Database.ExecuteSqlCommand("delete from Towns");
+
+            foreach (var town in DtSet.Tables[0].AsEnumerable())
+            {
+                currentDb.Towns.Add(new Town { Name = town[0].ToString() });
+                System.Console.Write("-");
+            }
+
+            System.Console.WriteLine();
+
+            currentDb.SaveChanges();
+
+            MyConnection.Close();
+
+        }
+    }
+}
---
+++ CinemaNetwork Second Edition/ExcelImporterConsole.Controller/ImporterEntryPoint.cs
@@ -0,0 +1,12 @@
+﻿namespace ExcelImporterConsole.Controller
+{
+    public class ImporterEntryPoint
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Importing started:");
+            ExcelImporter.ImportTowns("towns.xls");
+            System.Console.WriteLine("Imported completed successfully!");
+        }
+    }
+}
