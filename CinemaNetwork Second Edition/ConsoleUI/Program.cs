9e92dc299f4acde61e23d49aafe1fcb1210b0015
VenelinGP <venelin.petkov@netcoms.eu>
2015-10-20 02:40:18 +0300

    Creating JSON Reports

--- CinemaNetwork Second Edition/ConsoleUI/Program.cs
+++ CinemaNetwork Second Edition/ConsoleUI/Program.cs
@@ -1,21 +1,31 @@
 ﻿namespace ConsoleUI
 {
+    using System;
+    using CinemaNetwork.Models;
+    using Json.Contorller;
     using Mongo.Controller;
     using Pdf.Controller;
     using RandomGenerators;
-    using System;
     using Xml.Controller;
     using XmlReporter.Generators;
+
 
     class Program
     {
         static void Main()
         {
+            var db = new CinameNetworkEntities();
+            foreach (var m in db.Countries)
+            {
+                Console.WriteLine(m.Name);
+            }
+
             Console.WriteLine("Generate mongo data: 1");
             Console.WriteLine("Send mongo data to SQL: 2");
             Console.WriteLine("Generate and send random movies and generate xml: 3");
             Console.WriteLine("Generate PDF Report: 4");
             Console.WriteLine("Generate XML report: 5");
+            Console.WriteLine("Generate JSON files: 6");
 
             var mongoController = new MongoDbController("People", "mongodb://127.0.0.1");
             var xmlController = new XmlRandomDataGenerator(new RandomGenerator(), 10, 20);
@@ -45,6 +55,17 @@
                 else if (command == "5")
                 {
                     xmlReportGenerator.Generate("../../../xmlReport.xml");
+                }
+                else if (command == "6")
+                {
+                    var database = new CinameNetworkEntities();
+                    var exportToJson = new JsonExporter();
+                    exportToJson.CreateReportEntry(database.Movies);
+                    //// Tova gi zapisva v glavnata direktoria, ako triabva da se zapishat drugade reportite
+                    //// opravi path.
+                    var path = String.Empty;
+                    exportToJson.SaveReport(path);
+                    break;
                 }
             }
         }
